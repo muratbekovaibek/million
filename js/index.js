14102ce9451ac315c3baf583f8edd80fe51f6d2e
muratbekovaibek <muratbekov2709@gmail.com>
2020-06-22 10:40:33 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+
+//var yuo =prompt("hfbvjk")
+
+//if (name =="aibek" || name =="akyl"){
+    //alert ("mamm")
+//}else if (name=="akmaral"){
+    //alert("askdfh")
+//}
+//else if("ahbghflah"){
+    //alert("lkjs")
+//}
+//var name =prompt("ведите ваше имя")
+
+  //  alert("здаствуйте!" +name)
+
+//var q1 =prompt (name+ "плошадь Кыргызстана 1)196км2 2)156км2 3)322км2 4)198км2" )
+//if  (q1==4){
+  //  alert("вы идете правилно!")
+    //var q2=prompt(name+ "Сколько районов Бишкеке? 1)5 2)4 3)77 4)55")
+    //if   (q2==2){
+      //  alert("правилно! идем дальше")
+        //var q3=prompt("Столица Кыргызыстана 1)Ош 2)Баткен 3)Бишкек 4)Талас")
+        //if  (q3==3){
+          //  alert("молодец")
+            //var q4=prompt (name+"Где я учусь 1)мук 2)алатоо 3)юр 4)it-hub")
+            //if (q4==4){
+              //  alert("good")
+            //}else{
+              //  alert("К сожелению это не правда. учи сука географию")
+            //}
+        //}else{
+          //  alert("лоххх")
+        //}
+    //}else{
+      //  alert("лоххх")
+    //}
+//}else{
+    //alert("лоххх")
+//}
+
+
+var site='http://api/giphy.com/v1/girs/search?q={';
+var key='} &api_key=dc6zaTOxFJmzC';
+function go() {
+    var info =document.getElementById("info")
+    info.innerHTML=""
+
+
+    var name = document.getElementById("name").value
+    search (name)
+} 
+async function search(name) {
+    
+}
+
+
+
+
+
+
+
